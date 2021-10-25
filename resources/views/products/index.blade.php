0514bef17a3dfa9e4a2e1277c9b75d8cb581fc66
angel24r <17030621@itcelaya.edu.mx>
2021-10-24 23:31:09 -0500

    commit

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container"><br>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
@@ -20,9 +20,36 @@
                         <!--if es una funcion de blade, seleccionamos el nombre de la variable del with
                         si existe lo muestra-->
                         @if(session('info'))
-                            {{ session('info') }}
+                            <div class="alert alert-success">
+                                {{ session('info') }}
+                            </div>
                         @endif
-                        ...
+                        <table class="table table-hover table-sm">
+                            <thead>
+                                <th>Descripcion</th>
+                                <th>Precio</th>
+                                <th>Accion</th>
+                            </thead>                           
+                            <tbody>
+                                @foreach($products as $product)
+                                <tr>
+                                    <td>
+                                        {{ $product->description}}
+                                    </td>
+                                    <td>
+                                        {{ $product->price}}
+                                    </td>
+                                    <td>
+                                        <a href="javascript: document.getElementById('delete-{{ $product->id }}').submit()" class="btn btn-danger btn-sm">Eliminar</a>
+                                        <form id="delete-{{ $product->id }}" action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                            @method('delete')
+                                            @csrf
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 
 Route::get('products',function(){
-    return view('products.index');
+    $products = Product::orderBy('created_at','desc')->get();
+    
+    return view('products.index', compact('products'));
 })->name('products.index'); //Nombre de la ruta para usarla en html
 
 Route::get('products/create',function(){
@@ -17,8 +19,12 @@ Route::post('products',function(Request $request){
     $newProduct->description = $request->input('description');
     $newProduct->price = $request->input('price');
     $newProduct->save(); // guardar productos
-     
     //redireccionar a otra ruta with(nombrevariable,mensaje)
     return redirect()->route('products.index')->with('info','Producto creado exitosamente');
-
 })->name('products.store');//almacenar registros
+
+Route::delete('products/{id}',function($id){
+    $product = Product::findOrFail($id);
+    $product->delete();
+    return redirect()->route('products.index')->with('info','Producto eliminado exitosamente');
+})->name('products.destroy');
